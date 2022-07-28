f4cdbc15e04ccb0bc3f44b48d587a453fe27bc9f
betaluis <luis.betancourt.dev@gmail.com>
2022-07-28 09:51:17 +0000

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        "light-gray": "hsl(217, 12%, 63%)",
+        "medium-gray": "hsl(216, 12%, 54%)",
+        "dark-blue": "hsl(213, 19%, 18%)",
+        "darker-blue": "hsl(216, 12%, 8%)",
+      },
+    },
+  },
+  plugins: [],
+}
